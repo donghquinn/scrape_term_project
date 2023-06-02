f23b9db6fbc1911e880d042e619159494a7e0b79
Kim, Dong H <ehdgus1524@gmail.com>
2023-06-02 14:43:25 +0900

    wip

--- src/libraries/csv.lib.ts
+++ src/libraries/csv.lib.ts
@@ -4,7 +4,7 @@ import { Logger } from 'utils/logger.util';
 export const saveAsCSV = (title: string, category: string, field: string) => {
   const csv = `${title},${category},${field}\n`;
   try {
-    appendFileSync(`../../data/${Date.now() + 'naver.csv'}`, csv);
+    appendFileSync(`../data/${Date.now() + 'naver.csv'}`, csv);
   } catch (err) {
     Logger.error('Saving CSV Failed');
   }
