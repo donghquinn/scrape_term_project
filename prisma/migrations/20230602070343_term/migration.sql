2f81852c764098e7e42faa60e2a1e7113094eb31
Kim, Dong H <ehdgus1524@gmail.com>
2023-06-04 00:53:41 +0900

    wip

---
+++ prisma/migrations/20230602070343_term/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `NaverKin` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "NaverKin";
+
+-- CreateTable
+CREATE TABLE "naver" (
+    "uuid" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "create" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "naver_pkey" PRIMARY KEY ("uuid")
+);
